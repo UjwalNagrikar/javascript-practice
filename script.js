393d05c37b69835de69c3da34dacddc8382426c7
Ujwal Nagrikar <ujjwalnagrikar@mail.com>
2026-03-20 20:43:50 +0530

    Data types  in js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-console.log("Hello World");
-let a = 10;
-let b = 20;
-let c = a + b;
-console.log(c);
+// data types in java script
+
+// String = text
+
+let name = "Ujwal";
+console.log(name);
+
+// Number
+
+let age = 19;
+console.log("Age is ", age);
+
+// Array 
+
+let arr = [1, 2, 3, 4, 5];
+console.log("These is an array:", arr);
+
+// Object
+
+let person = {
+    name: "Ujwal",
+    age: 19,
+    city: "Nagpur"
+}
+console.log("This is an object:", person);
