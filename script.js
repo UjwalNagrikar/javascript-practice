67b64b5385d16f98870415cb4fd4d16a0c320505
Ujwal Nagrikar <ujjwalnagrikar@mail.com>
2026-03-20 20:56:45 +0530

    Operators in js

--- script.js
+++ script.js
@@ -23,3 +23,40 @@ let person = {
     city: "Nagpur"
 }
 console.log("This is an object:", person);
+
+
+// Operators
+
+// Arithmetic Operators
+let a = 10;
+let b = 20;
+console.log("Addition: ", a + b);
+console.log("Subtraction: ", a - b);
+console.log("Multiplication: ", a * b);
+console.log("Division: ", a / b);
+console.log("Modulus: ", a % b);
+
+// Assignment Operators
+let c = 5;
+c += 10;
+console.log("Assignment Operator (+=): ", c);
+
+// Comparison Operators
+console.log("Equal to (==): ", a == b);
+console.log("Not equal to (!=): ", a != b);
+console.log("Greater than (>): ", a > b);
+console.log("Less than (<): ", a < b);
+console.log("Greater than or equal to (>=): ", a >= b);
+console.log("Less than or equal to (<=): ", a <= b);
+
+// Logical Operators
+let x = true;
+let y = false;
+console.log("Logical AND (&&): ", x && y);
+console.log("Logical OR (||): ", x || y);
+console.log("Logical NOT (!): ", !x);
+
+// Increment and Decrement Operators
+let d = 5;
+console.log("Increment (d++): ", d++);
+console.log("Decrement (d--): ", d--);
