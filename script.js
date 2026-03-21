27cc1b68f4bccda5dfc95fd5c24c89663af84861
Ujwal Nagrikar <ujjwalnagrikar@mail.com>
2026-03-21 20:48:31 +0530

    Loops in js'

--- script.js
+++ script.js
@@ -1,112 +1,136 @@
-// data types in java script
+// // data types in java script
 
-// String = text
+// // String = text
 
-let name = "Ujwal";
-console.log(name);
+// let name = "Ujwal";
+// console.log(name);
 
-// Number
+// // Number
 
-let age = 19;
-console.log("Age is ", age);
+// let age = 19;
+// console.log("Age is ", age);
 
-// Array 
+// // Array 
 
-let arr = [1, 2, 3, 4, 5];
-console.log("These is an array:", arr);
+// let arr = [1, 2, 3, 4, 5];
+// console.log("These is an array:", arr);
 
-// Object
+// // Object
 
-let person = {
-    name: "Ujwal",
-    age: 19,
-    city: "Nagpur"
+// let person = {
+//     name: "Ujwal",
+//     age: 19,
+//     city: "Nagpur"
+// }
+// console.log("This is an object:", person);
+
+
+// // Operators
+
+// // Arithmetic Operators
+// let a = 10;
+// let b = 20;
+// console.log("Addition: ", a + b);
+// console.log("Subtraction: ", a - b);
+// console.log("Multiplication: ", a * b);
+// console.log("Division: ", a / b);
+// console.log("Modulus: ", a % b);
+
+// // Assignment Operators
+// let c = 5;
+// c += 10;
+// console.log("Assignment Operator (+=): ", c);
+
+// // Comparison Operators
+// console.log("Equal to (==): ", a == b);
+// console.log("Not equal to (!=): ", a != b);
+// console.log("Greater than (>): ", a > b);
+// console.log("Less than (<): ", a < b);
+// console.log("Greater than or equal to (>=): ", a >= b);
+// console.log("Less than or equal to (<=): ", a <= b);
+
+// // Logical Operators
+// let x = true;
+// let y = false;
+// console.log("Logical AND (&&): ", x && y);
+// console.log("Logical OR (||): ", x || y);
+// console.log("Logical NOT (!): ", !x);
+
+// // Increment and Decrement Operators
+// let d = 5;
+// console.log("Increment (d++): ", d++);
+// console.log("Decrement (d--): ", d--);
+
+
+// // Conditional Statements
+
+// // vote 
+
+// let age1 = 18;
+
+// if (age1 >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+// // even and odd
+// let num = 10;
+
+// if (num % 2 === 0) {
+//     console.log("The number is even.");
+// } else {
+//     console.log("The number is odd.");
+// }
+
+// // nested if
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//     console.log("Grade: A");
+// } else if (marks >= 80) {
+//     console.log("Grade: B");
+// } else if (marks >= 70) {
+//     console.log("Grade: C");
+// } else if (marks >= 60) {
+//     console.log("Grade: D");
+// } else {
+//     console.log("Grade: F");
+// }
+
+// // Conditional statements are used to execute different blocks of code based on whether a condition is true or false.
+
+// let time = 14;
+
+// if (time < 12) {
+//     console.log("Good morning!");
+// } else if (time < 18) {
+//     console.log("Good afternoon!");
+// } else {
+//     console.log("Good evening!");
+// }
+
+// Loops in js 
+
+// for loop
+
+for (let i = 0; i < 5; i++) {
+    console.log("For loop iteration: ", i);
 }
-console.log("This is an object:", person);
 
+// while loop
 
-// Operators
-
-// Arithmetic Operators
-let a = 10;
-let b = 20;
-console.log("Addition: ", a + b);
-console.log("Subtraction: ", a - b);
-console.log("Multiplication: ", a * b);
-console.log("Division: ", a / b);
-console.log("Modulus: ", a % b);
-
-// Assignment Operators
-let c = 5;
-c += 10;
-console.log("Assignment Operator (+=): ", c);
-
-// Comparison Operators
-console.log("Equal to (==): ", a == b);
-console.log("Not equal to (!=): ", a != b);
-console.log("Greater than (>): ", a > b);
-console.log("Less than (<): ", a < b);
-console.log("Greater than or equal to (>=): ", a >= b);
-console.log("Less than or equal to (<=): ", a <= b);
-
-// Logical Operators
-let x = true;
-let y = false;
-console.log("Logical AND (&&): ", x && y);
-console.log("Logical OR (||): ", x || y);
-console.log("Logical NOT (!): ", !x);
-
-// Increment and Decrement Operators
-let d = 5;
-console.log("Increment (d++): ", d++);
-console.log("Decrement (d--): ", d--);
-
-
-// Conditional Statements
-
-// vote 
-
-let age1 = 18;
-
-if (age1 >= 18) {
-    console.log("You are eligible to vote.");
-} else {
-    console.log("You are not eligible to vote.");
+let j = 0;
+while (j < 5) {
+    console.log("While loop iteration: ", j);
+    j++;
 }
 
-// even and odd
-let num = 10;
+// do-while loop
 
-if (num % 2 === 0) {
-    console.log("The number is even.");
-} else {
-    console.log("The number is odd.");
-}
-
-// nested if
-
-let marks = 85;
-
-if (marks >= 90) {
-    console.log("Grade: A");
-} else if (marks >= 80) {
-    console.log("Grade: B");
-} else if (marks >= 70) {
-    console.log("Grade: C");
-} else if (marks >= 60) {
-    console.log("Grade: D");
-} else {
-    console.log("Grade: F");
-}
-
-// Conditional statements are used to execute different blocks of code based on whether a condition is true or false.
-
-let time = 14;
-
-if (time < 12) {
-    console.log("Good morning!");
-} else if (time < 18) {
-    console.log("Good afternoon!");
-} else {
-    console.log("Good evening!");
-}
+let k = 0;
+do {
+    console.log("Do-While loop iteration: ", k);
+    k++;
+} while (k < 5);
